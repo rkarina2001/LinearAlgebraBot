3954ccf167e325d2e24b37f9b89dee5ce8730ddb
Anton <antoshka29062001@gmail.com>
2021-05-18 21:41:15 +0300

    Added CompareDet, Compare(matrix), MulOutput

--- TaskGenerator/src/TaskGenerator.java
+++ TaskGenerator/src/TaskGenerator.java
@@ -8,6 +8,50 @@ public class TaskGenerator {
     final int DET = 3;
     MatrixAllFunc m;
 
+    public String CompareDet(double a) { //Ввод определителя и его сравнение с результатом
+        Scanner s = new Scanner(System.in);
+        double b;
+        if (s.hasNextDouble()) {
+            b = s.nextDouble();
+        } else {
+            b = CheckFractionalNumber(s.next());
+        }
+        if (b == a) {
+            return "Good job!";
+        } else {
+            return "Incorrect.\nMust be: " + a + "\nFound: " + b;
+        }
+    }
+
+    public String Compare(double[][] A, boolean f) { //Ввод матрицы пользователя и её сравнение с результатом
+        Scanner s = new Scanner(System.in);
+        double[][] X = new double[A.length][A[0].length];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (!f) {
+                    if (s.hasNextDouble()) {
+                        X[j][i] = s.nextDouble();
+                    } else {
+                        X[j][i] = CheckFractionalNumber(s.next());
+                    }
+                    if (X[j][i] != A[j][i]) {
+                        return "Mistake in row: " + (i + 1) + ", col: " + (j + 1) + "\nMust be: " + A[j][i] + "\nFound: " + X[j][i];
+                    }
+                } else {
+                    if (s.hasNextDouble()) {
+                        X[i][j] = s.nextDouble();
+                    } else {
+                        X[i][j] = CheckFractionalNumber(s.next());
+                    }
+                    if (X[i][j] != A[i][j]) {
+                        return "Mistake in row: " + (i + 1) + ", col: " + (j + 1) + "\nMust be: " + A[i][j] + "\nFound: " + X[i][j];
+                    }
+                }
+            }
+        }
+        return "Good job!";
+    }
+
     public double CheckFractionalNumber(String fractional) { //Обработка числа в дробном формате
         int slash = fractional.indexOf('/');
         double top = Double.parseDouble(fractional.substring(0, slash));
@@ -23,6 +67,15 @@ public class TaskGenerator {
             }
         }
         return A;
+    }
+
+    public void MulOutput(double[][] A) { //Вывод матрицы для операции умножения
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[A.length - 1].length; j++) {
+                System.out.printf("%1$-3s", (int) A[i][j]);
+            }
+            System.out.print("\n");
+        }
     }
 
     public void MatrixOutput(double[][] A, boolean f) { //Вывод матрицы
